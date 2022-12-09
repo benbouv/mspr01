e726bfe61183f11d11ccdeb7ec2bed69c0017bed
on99833 <on99833@outlook.fr>
2022-12-09 11:51:57 +0100

    test

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends AbstractController
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
     {
-        // controller can be blank: it will never be called!
+        // controller can be blank: it will never be called
         throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
