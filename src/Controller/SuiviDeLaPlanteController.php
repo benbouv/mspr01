581bd2cec862e9ebe4b6653c05bceee610d4b5fa
on99833 <on99833@outlook.fr>
2023-01-02 01:10:50 +0100

    crud message et photo + sécurité up

--- src/Controller/SuiviDeLaPlanteController.php
+++ src/Controller/SuiviDeLaPlanteController.php
@@ -11,6 +11,7 @@ use App\Repository\PhotoRepository;
 use App\Repository\PlanteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,7 +43,7 @@ class SuiviDeLaPlanteController extends AbstractController
     public function index(Plante $plante): Response
     {
         $user = $this->security->getUser();
-        if(!empty($user))
+        if($this->UserIsPlanteAllowed($user,$plante))
         {
             $photos = $this->repository_photo->findByPlanteId($plante->getId());
             $photos = array_reverse($photos);
@@ -55,8 +56,15 @@ class SuiviDeLaPlanteController extends AbstractController
                 'messagess' => $messages
             ]);
         }
+        else
+        {
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->redirectToRoute('app_register');
     }
+
+
 
 
     // ADD MESSAGE \\
@@ -88,6 +96,65 @@ class SuiviDeLaPlanteController extends AbstractController
         ]);
     }
 
+    // SUPPR MESSAGE \\
+
+    #[Route('/user/mesplantes/SuiviDeLaPlante{id}/supprmessage{message_id}', name: 'app_suiviplante_supprmessage')]
+    #[ParamConverter('message', options: ['mapping' => ['message_id' => 'id']])]
+    public function supprmessage(Plante $plante, Message $message)
+    {
+        $user = $this->security->getUser();
+        if($this->UserIsPlanteAllowed($user,$plante))
+        {
+            if($message->getUserWritingMessage()===$user)
+            {
+                $this->repository_messages->remove($message,true);
+            }
+            else
+            {
+                $this->addFlash('AddMessageNonPermis','Non autorisé. Seul l\'utilisateur responsable du message peut le supprimer à partir de la page.');
+            }
+        }
+        else
+        {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->redirectToRoute('app_suiviplante', [
+            'id' => $plante->getId()
+        ]);
+    }
+
+    // EDIT MESSAGE \\
+
+    #[Route('/user/mesplantes/SuiviDeLaPlante{id}/editmessage{message_id}', name: 'app_suiviplante_editmessage')]
+    #[ParamConverter('message', options: ['mapping' => ['message_id' => 'id']])]
+    public function editmessage(Plante $plante, Message $message)
+    {
+        $user = $this->security->getUser();
+        if($this->UserIsPlanteAllowed($user,$plante))
+        {
+            if($message->getUserWritingMessage()===$user)
+            {
+                $message->setContenu($_POST['infomessage']);
+                $this->repository_messages->save($message,true);
+
+            }
+            else
+            {
+                $this->addFlash('AddMessageNonPermis','Non autorisé. Seul l\'utilisateur responsable du message peut le modifier à partir de la page.');
+            }
+        }
+        else
+        {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->redirectToRoute('app_suiviplante', [
+            'id' => $plante->getId()
+        ]);
+    }
+
+
 
 
     // ADD PHOTO \\
@@ -98,57 +165,98 @@ class SuiviDeLaPlanteController extends AbstractController
         $user = $this->security->getUser();
         if($this->UserIsPlanteAllowed($user,$plante))
         {
-            $photo = new Photo;
-            $form = $this->createForm(PhotoentrerType::class, $photo);
-            $form->handleRequest($request);
-
-            if($form->isSubmitted() && $form->isValid())
+            if($plante->getUserOwningPlant()===$user || $plante->getUserKeepingPlant()===$user)
             {
-                $image = $form->get('name')->getData();
-                if ($image !== null)
+                $photo = new Photo;
+                $form = $this->createForm(PhotoentrerType::class, $photo);
+                $form->handleRequest($request);
+
+                if($form->isSubmitted() && $form->isValid())
                 {
-                    $fichier_nom = md5( uniqid("image")) . '.' . $image->guessExtension();
-                    $fichier_directory = $this->getParameter('images_directory');
-                    //supression de l'ancienne image
-                    if (is_file($fichier_directory . '/' . $photo->getName()) && $photo->getName() !== null)
+                    $image = $form->get('name')->getData();
+                    if ($image !== null)
                     {
-                        unlink($fichier_directory . '/' . $photo->getName());
+                        $fichier_nom = md5( uniqid("image")) . '.' . $image->guessExtension();
+                        $fichier_directory = $this->getParameter('images_directory');
+                        //supression de l'ancienne image
+                        if (is_file($fichier_directory . '/' . $photo->getName()) && $photo->getName() !== null)
+                        {
+                            unlink($fichier_directory . '/' . $photo->getName());
+                        }
+                        //ajout de la nouvelle photo
+                        $image->move( $fichier_directory, $fichier_nom);
+                        $photo->setName($fichier_nom);
+                        $photo->setDate(new \DateTime());
+                        $photo->setPlantePossedePhoto($plante);
+                        //set l'image de la carte de la plante, si pas d'image
+                        if($plante->getImage()===null)
+                        {
+                            $plante->setImage($fichier_nom);
+                        }
+                        $this->repository_photo->save($photo,true);
                     }
-                    //ajout de la nouvelle photo
-                    $image->move( $fichier_directory, $fichier_nom);
-                    $photo->setName($fichier_nom);
-                    $photo->setDate(new \DateTime());
-                    $photo->setPlantePossedePhoto($plante);
-                    //set l'image de la carte de la plante, si pas d'image
-                    if($plante->getImage()===null)
-                    {
-                        $plante->setImage($fichier_nom);
-                    }
-                    $this->repository_photo->save($photo,true);
+                    return $this->redirectToRoute('app_suiviplante', [
+                        'id' => $plante->getId()
+                    ]);
                 }
-                return $this->redirectToRoute('app_suiviplante', [
-                    'id' => $plante->getId()
+                return $this->render('MesPlantes/MaPlanteAddPhoto.html.twig',[
+                    'form' => $form->createView()
                 ]);
             }
-            return $this->render('MesPlantes/MaPlanteAddPhoto.html.twig',[
-                'form' => $form->createView()
-            ]);
+            else
+            {
+                $this->addFlash('AddPhotoNonPermise','Non autorisé. Seul les utilisateurs responsables de la plante peuvent ajouter une photo.');
+            }
         }
         else
         {
-            $this->addFlash('AddPhotoNonPermise','Non autorisé. Seul les utilisateurs responsables de la plante peuvent ajouter une photo.');
+            return $this->redirectToRoute('app_home');
         }
+
         return $this->redirectToRoute('app_suiviplante', [
             'id' => $plante->getId()
         ]);
     
     }
 
+    // SUPPR PHOTO \\
+
+    #[Route('/user/mesplantes/SuiviDeLaPlante{id}/supprphoto{photo_id}', name: 'app_suiviplante_supprphoto')]
+    #[ParamConverter('photo', options: ['mapping' => ['photo_id' => 'id']])]
+    public function supprphoto(Plante $plante, Photo $photo)
+    {
+        $user = $this->security->getUser();
+        if($this->UserIsPlanteAllowed($user,$plante))
+        {
+            if($plante->getUserOwningPlant()===$user || $plante->getUserKeepingPlant()===$user)
+            {
+                $fichier_directory = $this->getParameter('images_directory');
+                if (is_file($fichier_directory . '/' . $photo->getName()) && $photo->getName() !== null)
+                {
+                    unlink($fichier_directory . '/' . $photo->getName());
+                }
+                $this->repository_photo->remove($photo,true);
+            }
+            else
+            {
+                $this->addFlash('AddPhotoNonPermise','Non autorisé. Seul les utilisateurs responsables de la plante peuvent supprimer une photo.');
+            }
+        }
+        else
+        {
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->redirectToRoute('app_suiviplante', [
+            'id' => $plante->getId()
+        ]);
+    }
+
 
     //user is allowed if gardien ou proprio de la plante
     private function UserIsPlanteAllowed($user,$plante): bool
     {
-        if($user->getPlantesKept()===$plante || $plante->getUserOwningPlant()===$user)
+        if($plante->getUserKeepingPlant()===$user || $plante->getUserOwningPlant()===$user)
         {
             return true;
         }
